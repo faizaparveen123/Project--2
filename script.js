9dd980cd25b8164971df6ed129a3ab194a604fff
faizaparveen123 <faizaparveen@aptechnorth.edu.pk>
2025-02-21 19:37:06 +0500

    create navbar

---
+++ script.js
@@ -0,0 +1,15 @@
+// function tocelsius(fahrenheit) {
+//     return(5/9) * (fahrenheit-32);
+// }
+// let x = tocelsius(77);
+// let text = "the temperature is " + x + " celsius";
+// alert(text)
+
+// function convertor() {
+//     let f = number(document.getelementbyid("fah").value)
+//     let c = 5/9 * (f - 32)
+//     document.getelementbyid("temp").innerhtml = c
+// }
+// var  d = new Date();
+// console.log(d)
+
